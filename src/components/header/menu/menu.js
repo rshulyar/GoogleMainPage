1ea462bc7676e2d45ded5914e26e39cd25203634
rshulyar <39058350+rshulyar@users.noreply.github.com>
2021-09-04 21:24:10 +0300

    1

---
+++ src/components/header/menu/menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './menu.css';
+
+function Menu () {
+    return (
+        <div>
+            {/* button */}
+            {/* menu */}
+        </div>
+    )
+}
+
+export default Menu;
--- src/config.js
+++ src/config.js
@@ -1,6 +1,42 @@
 const config = {
     gmailLink: 'https://gmail.com',
-    imagesLink: 'https://www.google.com.ua/imghp?hl=uk&ogbl'
+    imagesLink: 'https://www.google.com.ua/imghp?hl=uk&ogbl',
+    menu1: [
+        ['Обліковий запис', 'https://myaccount.google.com/?utm_source=OGB&utm_medium=app', ''],
+        ['Пошук', 'https://www.google.com.ua/webhp', ''],
+        ['Карти', 'https://maps.google.com.ua/maps?hl=uk', ''],
+        ['Youtube', 'https://www.youtube.com/?gl=UA', ''],
+        ['Новини', 'https://news.google.com/', ''],
+        ['Gmail', 'https://mail.google.com/mail/', ''],
+        ['Meet', 'https://meet.google.com?hs=197&pli=1&authuser=0', ''],
+        ['Чат', 'https://chat.google.com/', ''],
+        ['Контакти', 'https://contacts.google.com/?hl=uk', ''],
+        ['Диск', 'https://drive.google.com/?authuser=0', ''],
+        ['Календар', 'https://calendar.google.com/calendar', ''],
+        ['Перекладач', 'https://translate.google.com.ua/?hl=uk', ''],
+        ['Фото', 'https://photos.google.com/?pageId=none', ''],
+        ['Duo', 'https://duo.google.com/?usp=duo_ald', '']
+    ], // для меню використовувати вкладені масиви [[nameElement][link][sprite coordinates]]
+    menu2: [
+        ['Документи', 'https://docs.google.com/document/?usp=docs_alc&authuser=0', ''],
+        ['Таблиці', 'https://docs.google.com/spreadsheets/?usp=sheets_alc&authuser=0', ''],
+        ['Презентації', 'https://docs.google.com/presentation/?usp=slides_alc&authuser=0', ''],
+        ['Книги', 'https://books.google.com.ua/?hl=uk', ''],
+        ['Blogger', 'https://www.blogger.com/', ''],
+        ['Hangouts', 'https://hangouts.google.com/', ''],
+        ['Keep', 'https://keep.google.com/u/0', ''],
+        ['Jumboard', 'https://jamboard.google.com/?authuser=0&usp=jam_ald', ''],
+        ['Клас', 'https://classroom.google.com/?authuser=0', ''],
+        ['Земля', 'https://earth.google.com/web/', ''],
+        ['Колекції', 'https://www.google.com.ua/save', ''],
+        ['Arts and culture', 'https://artsandculture.google.com/?hl=uk&utm_source=ogs.google.com&utm_medium=referral&authuser=0', ''],
+        ['Google ads', 'https://ads.google.com/home/?subid=ww-ww-et-g-aw-a-vasquette_ads_cons_1!o2', ''],
+        ['Подкасти', 'https://podcasts.google.com/', ''],
+        ['Google one', 'https://one.google.com/', ''],
+        ['Поїздки', 'https://www.google.com/travel/?dest_src=al', ''],
+        ['Форми', 'https://docs.google.com/forms/?authuser=0&usp=forms_alc', '']
+    ],
+    menuLink: ''
 };
 
 export default config;
