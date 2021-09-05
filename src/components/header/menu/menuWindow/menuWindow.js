4a027e1a30bf364ab32bd81e6ab28d5d1835e5fe
rshulyar <39058350+rshulyar@users.noreply.github.com>
2021-09-05 23:22:55 +0300

    3

--- src/components/header/menu/menuWindow/menuWindow.js
+++ src/components/header/menu/menuWindow/menuWindow.js
@@ -7,28 +7,33 @@ function MenuWindow() {
 
     let config1 = config.menu1;
 
+    const divMenuWindow = document.querySelector('.menuWindow');
+
+    // function spawnMenuWindowElements() {
+    //     let config1Length = config1.length;
+    //     let reverseArrNum = config1.length - 1;
+
+    //     for (let i = 0; i < config1Length; i++) {
+    //         divMenuWindow.insertAdjacentHTML('afterbegin', <MenuWindowElement/>);
+    //         reverseArrNum--;
+    //     }
+    // }
+
+    //spawnMenuWindowElements();
+
+    let config1Length = config1.length;
+    let reverseArrNum = config1.length - 1;
+
+    const result = [];
+    for (let i = 0; i < config1Length; i++) {
+        result.push(<MenuWindowElement/>)
+        reverseArrNum--;
+    }
     return (
         <div className='menuWindow'>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
-            <MenuWindowElement/>
+            {
+        result
+        }
         </div>
     )
 }
