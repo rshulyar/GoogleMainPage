a2cf371bf568bc094e471632f6d9fe800a34e687
rshulyar <39058350+rshulyar@users.noreply.github.com>
2021-09-07 19:21:38 +0300

    1

--- src/components/header/menu/menuButton/menuButton.js
+++ src/components/header/menu/menuButton/menuButton.js
@@ -6,9 +6,10 @@ function MenuButton(props) {
 
     return (
         <div className="div-menu-button" onClick={props.click}>
-            <a /* href="#" */ className="a-menu-button">
+            <a className="a-menu-button">
                 <img src={menuButtonImg} className="img-menu-button" ></img>
             </a>
+            <span className="hint">Додатки Google</span>
         </div>
     )
 }
