719311650b235f1cb2e36238e065b728d79c38b9
rshulyar <39058350+rshulyar@users.noreply.github.com>
2021-09-15 11:30:40 +0300

    final fix

--- src/components/searchPanel/buttons/buttons.js
+++ src/components/searchPanel/buttons/buttons.js
@@ -5,8 +5,8 @@ function Buttons() {
 
     return (
         <div className={styles["wrap"]}>
-            <input className={`${styles["button"]} ${styles["search-google"]}`} type="submit" value="Пошук Google"/>
-            <input className={`${styles["button"]} ${styles["luky-me"]}`} type="submit" value="Мені пощастить"/>
+            <input className={`${styles["button"]} ${styles["search-google"]}`} type="button" value="Пошук Google"/>
+            <input className={`${styles["button"]} ${styles["luky-me"]}`} type="button" value="Мені пощастить"/>
         </div>
     );
 }
